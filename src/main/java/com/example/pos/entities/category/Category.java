86600bb5d96964d933301867a42d440f3cb0252c
NguyenLam1708 <lam170804@gmail.com>
2025-12-24 18:13:19 +0700

    feat: create entity Category

---
+++ src/main/java/com/example/pos/entities/category/Category.java
@@ -0,0 +1,24 @@
+package com.example.pos.entities.category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String name;
+    String description;
+}
