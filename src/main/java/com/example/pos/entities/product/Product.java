cf069c129d5eb0e546e7ab7af1d92448da3dbea6
NguyenLam1708 <lam170804@gmail.com>
2025-12-24 18:15:05 +0700

    feat: create entity Product

---
+++ src/main/java/com/example/pos/entities/product/Product.java
@@ -0,0 +1,23 @@
+package com.example.pos.entities.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "Product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID )
+    String id;
+    String name;
+    String categoryId;
+    long price;
+}
