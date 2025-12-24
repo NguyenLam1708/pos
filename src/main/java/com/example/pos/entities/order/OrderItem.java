061bfe3cd874560dc26e50b7aeaa2041311d42b4
NguyenLam1708 <lam170804@gmail.com>
2025-12-24 18:10:18 +0700

    feat: create entity OrderItem

---
+++ src/main/java/com/example/pos/entities/order/OrderItem.java
@@ -0,0 +1,28 @@
+package com.example.pos.entities.order;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "order_id", nullable = false)
+    String orderId;
+
+    @Column(name = "product_id", nullable = false)
+    String productId;
+
+    int quantity;
+}
