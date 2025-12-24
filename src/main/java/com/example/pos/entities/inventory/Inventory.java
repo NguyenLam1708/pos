c6ef825640df5a729ab1fd41eda62bb315b3d245
NguyenLam1708 <lam170804@gmail.com>
2025-12-24 18:13:30 +0700

    feat: create entity Inventory

---
+++ src/main/java/com/example/pos/entities/inventory/Inventory.java
@@ -0,0 +1,26 @@
+package com.example.pos.entities.inventory;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "product_id",nullable = false)
+    String productId;
+
+    int totalQuantity; // Tổng tồn
+    int availableQuantity; // có thể bán
+}
